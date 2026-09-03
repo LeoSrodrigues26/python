110f04fbf516cff8feb014142a9a37a1f943512c
leonardosoares <leonardosrhumder@gmail.com>
2026-09-03 07:56:36 -0300

    Add files via upload

---
+++ 1CCPG-Python-FIAP-2026-main/aula07-dicionarios-tuplas/aula07a-dicionarios.py
@@ -0,0 +1,45 @@
+from pydoc import doc
+
+eng2sp = dict()
+print(eng2sp)
+
+eng2sp['one'] = 'uno'
+print(eng2sp)
+
+eng2sp = {
+    'one': 'uno',
+    'two': 'dos',
+    'three': 'tres'
+}
+print(eng2sp)
+print(eng2sp['two'])
+
+print('dos' in eng2sp)
+
+valores = eng2sp.values()
+print('uno' in valores)
+
+print(eng2sp.items())
+
+for chave, valor in eng2sp.items():
+    if valor == "uno":
+        print(chave)
+
+print()
+
+def count_letters(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+dict_contagem = count_letters("ovo")
+print(dict_contagem)
+
+
+
+
+
